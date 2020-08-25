c0b9985507ab5d53594a09b30bfbc62fdd98cb75
Nathaniel Millard <nathanielmillard@gmail.com>
2020-08-25 17:03:43 -0600

    Add method for accessing specific day oz

--- src/hydrationRepository.js
+++ src/hydrationRepository.js
@@ -4,10 +4,13 @@ class HydrationRepository {
   }
   calculateAverageOzAllTime(id){
     let daysById = this.data.filter((dataDay)=> dataDay.userID === id)
-    let totalOz = daysById.reduce(((ozTotal, dataDay)=>
-      ozTotal + dataDay.numOunces), 0)
+    let totalOz = daysById.reduce(((ozTotal, dataDay)=> ozTotal + dataDay.numOunces), 0)
     let average = (totalOz / daysById.length) * 10;
     return Math.round(average)/10
+  }
+  returnDaysHydration(id, date){
+    let dayData = this.data.find((dataDay) => dataDay.userID === id && dataDay.date === date)
+    return dayData.numOunces
   }
 }
 
--- test/hydrationRepository-test.js
+++ test/hydrationRepository-test.js
@@ -20,4 +20,8 @@ describe('HydrationRepository', () => {
   it('Should be able to calculate average oz all time by user ID', () => {
     expect(hydrationRepo.calculateAverageOzAllTime(1)).to.equal(32.3)
   })
+
+  it('Should be able to return oz on specific day for user', ()=> {
+    expect(hydrationRepo.returnDaysHydration(1, "2019/06/6")).to.equal(30)
+  })
 })
