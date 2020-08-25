ff1e4f50ca49c41e25e2275f705c27b1327363e1
Orlando Murcio <ato.murcio@gmail.com>
2020-08-25 15:05:51 -0600

    Display user's information by instantiating user class

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,38 @@
-console.log("Hello World");
+
+
+const cardName = document.querySelector(".card-name");
+const address = document.querySelector(".address");
+const email = document.querySelector(".email");
+const dailyStepGoal = document.querySelector(".daily-step-goal");
+const welMessage = document.querySelector(".wel-message")
+
+let trialData = { 
+    "id": 2,
+    "name": "Christopher Jonson",
+    "address": "123 5th Street, Denver CO 12345",
+    "email": "Christopher@hotmail.com",
+    "strideLength": 4,
+    "dailyStepGoal": 20000,
+    "friends": [ 1, 3, 4] 
+};
+
+let user = new User(trialData)
+
+let loadCardInfo = (user) => {
+    cardName.innerText = user.name;
+    address.innerText = user.address;
+    email.innerText = user.email;
+    dailyStepGoal.innerText = user.dailyStepGoal
+}
+
+let updateWelcomeMessage = (user) => {
+    welMessage.innerText = `Welcome ${user.getFirstName()}! Let's have another great day!`
+}
+
+let loadUserData = (user) => {
+    loadCardInfo(user);
+    updateWelcomeMessage(user);
+}
+
+
+window.onload = loadUserData(user)
