639850bc26f589c81798b4884e2b5501594ebc11
Orlando Murcio <ato.murcio@gmail.com>
2020-08-26 19:35:17 -0600

    Add parameters to chartTemplate

--- src/chart.js
+++ src/chart.js
@@ -1,11 +1,11 @@
 class ChartTemplate {
-    constructor(type, title, data, border) {
-    this.type = type,
+    constructor(type, labels, title, data, border) {
+    this.type = type,//needs to be a string 
     this.data = {
-        labels: [],
+        labels: labels || [], //need to be an array
         datasets: [{
             label: title,
-            data: data,
+            data: data || [], //needs to be an array
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -14,7 +14,14 @@ class ChartTemplate {
                 'rgba(153, 102, 255, 0.2)',
                 'rgba(255, 159, 64, 0.2)'
             ],
-            borderColor: ['', '', '', '', ''],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
             borderWidth: border
         }]
     },
