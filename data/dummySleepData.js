d57989f1f643db813c0db25df9c50325d632d390
Nathaniel Millard <nathanielmillard@gmail.com>
2020-08-27 14:49:18 -0600

    Create dummy sleep data

---
+++ data/dummySleepData.js
@@ -0,0 +1,166 @@
+const sleepTestData = [
+    {
+      "userID": 1,
+      "date": "2020/08/14",
+      "hoursSlept": 6.5,
+      "sleepQuality": 4.2
+    },
+    {
+      "userID": 2,
+      "date": "2020/8/14",
+      "hoursSlept": 8.7,
+      "sleepQuality": 1.6
+    },
+    {
+      "userID": 3,
+      "date": "2020/8/14",
+      "hoursSlept": 4.8,
+      "sleepQuality": 5
+    },
+    {
+      "userID": 1,
+      "date": "2020/8/15",
+      "hoursSlept": 6.8,
+      "sleepQuality": 2
+    },
+    {
+      "userID": 2,
+      "date": "2020/8/15",
+      "hoursSlept": 10.7,
+      "sleepQuality": 3.3
+    },
+    {
+      "userID": 3,
+      "date": "2020/8/15",
+      "hoursSlept": 8.2,
+      "sleepQuality": 4
+    },
+    {
+      "userID": 1,
+      "date": "2020/8/16",
+      "hoursSlept": 8.8,
+      "sleepQuality": 1.8
+    },
+    {
+      "userID": 2,
+      "date": "2020/8/16",
+      "hoursSlept": 7.4,
+      "sleepQuality": 2.7
+    },
+    {
+      "userID": 3,
+      "date": "2020/8/16",
+      "hoursSlept": 9.9,
+      "sleepQuality": 4.6
+    },
+    {
+      "userID": 1,
+      "date": "2020/8/17",
+      "hoursSlept": 6.3,
+      "sleepQuality": 3.2
+    },
+    {
+      "userID": 2,
+      "date": "2020/8/17",
+      "hoursSlept": 9.5,
+      "sleepQuality": 1.7
+    },
+    {
+      "userID": 3,
+      "date": "2020/8/17",
+      "hoursSlept": 8.4,
+      "sleepQuality": 4.8
+    },
+    {
+      "userID": 1,
+      "date": "2020/8/18",
+      "hoursSlept": 4.1,
+      "sleepQuality": 2.2
+    },
+    {
+      "userID": 2,
+      "date": "2020/8/18",
+      "hoursSlept": 4.8,
+      "sleepQuality": 3.8
+    },
+    {
+      "userID": 3,
+      "date": "2020/8/18",
+      "hoursSlept": 4.7,
+      "sleepQuality": 3.8
+    },
+    {
+      "userID": 1,
+      "date": "2020/8/19",
+      "hoursSlept": 9.9,
+      "sleepQuality": 1.7
+    },
+    {
+      "userID": 2,
+      "date": "2020/8/19",
+      "hoursSlept": 10.6,
+      "sleepQuality": 1.6
+    },
+    {
+      "userID": 3,
+      "date": "2020/8/19",
+      "hoursSlept": 7.4,
+      "sleepQuality": 4.7
+    },
+    {
+      "userID": 1,
+      "date": "2020/8/20",
+      "hoursSlept": 8.6,
+      "sleepQuality": 2.6
+    },
+    {
+      "userID": 2,
+      "date": "2020/8/20",
+      "hoursSlept": 8.5,
+      "sleepQuality": 4.3
+    },
+    {
+      "userID": 3,
+      "date": "2020/8/20",
+      "hoursSlept": 4.5,
+      "sleepQuality": 3.7
+    },
+    {
+      "userID": 1,
+      "date": "2020/8/21",
+      "hoursSlept": 10.7,
+      "sleepQuality": 4
+    },
+    {
+      "userID": 2,
+      "date": "2020/8/21",
+      "hoursSlept": 8.2,
+      "sleepQuality": 1.9
+    },
+    {
+      "userID": 3,
+      "date": "2020/8/21",
+      "hoursSlept": 6.1,
+      "sleepQuality": 3.5
+    },
+    {
+      "userID": 1,
+      "date": "2020/8/22",
+      "hoursSlept": 4.6,
+      "sleepQuality": 1.4
+    },
+    {
+      "userID": 2,
+      "date": "2020/8/22",
+      "hoursSlept": 5.3,
+      "sleepQuality": 4.6
+    },
+    {
+      "userID": 3,
+      "date": "2020/8/22",
+      "hoursSlept": 5.3,
+      "sleepQuality": 4.4
+    },
+  ];
+  module.exports = sleepTestData;
+  
