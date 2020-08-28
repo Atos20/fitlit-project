97d13ed745e30d3d2ac591081a8afcf97ef70845
Nathaniel Millard <nathanielmillard@gmail.com>
2020-08-28 13:37:11 -0600

    Build out code for weeksGoodSleepers functionality

--- src/sleepRepository.js
+++ src/sleepRepository.js
@@ -63,7 +63,7 @@ class SleepRepository {
   }
 
   weeksGoodSleepers(date) {
-    const allUsersAndData = this.data.reduce((acc, obj) => {
+    let allUsersAndData = this.data.reduce((acc, obj) => {
       let key = obj.userID
       if (!acc[key]) {
         acc[key] = []
@@ -71,7 +71,38 @@ class SleepRepository {
       acc[key].push(obj)
       return acc
     },{})
+    console.log(allUsersAndData)
+    const numUsers = Object.keys(allUsersAndData)
+    console.log(numUsers)
+    const user1Date = (entry) => entry.date === date
+    console.log(allUsersAndData['1'])
+    const dateIndex = allUsersAndData['1'].findIndex(user1Date)
+    console.log(dateIndex)
+    numUsers.forEach((item) => {
+      allUsersAndData[item] = allUsersAndData[item].slice(dateIndex - 6, dateIndex + 1)
+      allUsersAndData[item] = allUsersAndData[item].reduce((sum, day) => {
+        return sum + day.sleepQuality
+      }, 0)
+      allUsersAndData[item] = allUsersAndData[item]/7
+    })
+    console.log(allUsersAndData)
+    const allHours = this.data.reduce((sum, entry) => {
+      return sum + entry.sleepQuality
+    },0)
+    const averageQualityThisWeekAllUsers = Object.entries(allUsersAndData)
+    console.log(averageQualityThisWeekAllUsers)
+    averageQualityThisWeekAllUsers.sort((a,b) => {
+        return b[1] - a[1]
+    })
+    console.log(averageQualityThisWeekAllUsers)
+    let averagingOver3 = []
+    averageQualityThisWeekAllUsers.forEach((user)=> {
+      if (user[1] > 3) averagingOver3.push(user[0])
+    })
+    return averagingOver3
   }
+  //Would like this to pass a test where expected end result is numbers not strings
+  //obviously this is a hefty chunck of code I want to pare down.
 
   topSleeper(date){
     const allOnDay = this.data.filter((entry) => {
