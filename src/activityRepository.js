e28e1348167e8745b06c8a0ccf548cf8214f1193
Orlando Murcio <ato.murcio@gmail.com>
2020-08-28 15:32:12 -0600

    Write method that returns the miles a user has walked by a given date

--- src/activityRepository.js
+++ src/activityRepository.js
@@ -1,8 +1,22 @@
 class ActivityRepository {
-    constructor(data) {
-        this.data = data;
-    }
-}
-if (typeof module !== 'undefined') {
-    module.exports = ActivityRepository;
+  constructor(activityData) {
+      this.data = activityData;
   }
+  
+  getMilesPerDay(user, date){
+    const usersActivity = this.data.find(data => data.userID === user.id && data.date === date);
+    console.log(usersActivity)
+    console.log(user)
+    const numberStepsPerMile = 5280 / user.strideLength;
+    console.log('steps per mile',numberStepsPerMile)
+    const userDistanceinMiles = usersActivity.numSteps / numberStepsPerMile
+    console.log(userDistanceinMiles)
+    return Math.round(userDistanceinMiles * 10) / 10 //=> 6 total
+  }
+
+  
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = ActivityRepository;
+}
