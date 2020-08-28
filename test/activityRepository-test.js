2f4e105e4d15faf9bd6c6e34e9efc18ed28c0dcd
Orlando Murcio <ato.murcio@gmail.com>
2020-08-28 10:43:00 -0600

    Set up require variables for dummy activity data and dummy user data

---
+++ test/activityRepository-test.js
@@ -0,0 +1,6 @@
+const chai = require('chai');
+const expect = chai.expect;
+// const moment = require('moment')
+
+const User = require('../src/user');
+const activityRepository = require('../data/dummyActivityData.js');
