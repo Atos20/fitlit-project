ac6ed52b82e2ad4e967941dc86d4b5c6f4952034
Orlando Murcio <ato.murcio@gmail.com>
2020-08-28 18:14:13 -0600

    Write methos that return the names of the best minutes active

--- src/activityRepository.js
+++ src/activityRepository.js
@@ -6,9 +6,9 @@ class ActivityRepository {
   getMilesPerDay(user, date){
     const usersActivity = this.data.find(data => data.userID === user.id && data.date === date);
     const numberStepsPerMile = 5280 / user.strideLength;
-    console.log('steps per mile',numberStepsPerMile)
+    // console.log('steps per mile',numberStepsPerMile)
     const userDistanceinMiles = usersActivity.numSteps / numberStepsPerMile
-    console.log(userDistanceinMiles)
+    // console.log(userDistanceinMiles)
     return Math.round(userDistanceinMiles * 10) / 10 //=> 6 total
   }
 
@@ -53,13 +53,13 @@ class ActivityRepository {
     // console.log(user.dailyStepGoal)
     //  console.log(getBestDatesInfo )
     const justDates = getBestDatesInfo.map( date => date.date);
-    console.log(justDates)
+    // console.log(justDates)
     return justDates
   }
 
   bestSatirClimbRecord(user) {
     const retrievedUsersInfo = this.data.filter(userInfo => userInfo.userID === user.id);
-    console.log(retrievedUsersInfo )
+    // console.log(retrievedUsersInfo )
     const bestRecord = retrievedUsersInfo.reduce((record, activityInfo) => {
       if (activityInfo.flightsOfStairs > record.bestClimb) {
         record.bestClimb = activityInfo.flightsOfStairs
@@ -68,8 +68,60 @@ class ActivityRepository {
     },{bestClimb: 0})
       return bestRecord.bestClimb
   }
+
+  averageAllUserActivities(date) {
+    const retrieveInfoByDate = this.data.filter(data => data.date === date)
+    // console.log(retrieveInfoByDate)
+    const averageData = retrieveInfoByDate.reduce((averageList, data)=> {
+      averageList.numStepsAverage += data.numSteps 
+      averageList.numStepsAverage = Math.round(averageList.numStepsAverage / retrieveInfoByDate.length)
+      
+      averageList.minutesActiveAverage += data.minutesActive
+      averageList.minutesActiveAverage =   Math.round(averageList.minutesActiveAverage/ retrieveInfoByDate.length)
+  
+      averageList.flightsOfStairsAverage += data.flightsOfStairs
+      averageList.flightsOfStairsAverage =  Math.round(averageList.flightsOfStairsAverage / retrieveInfoByDate.length)
+      return averageList
+    }, {numStepsAverage: 0, minutesActiveAverage: 0, flightsOfStairsAverage : 0, })
+    return averageData
+  }
+  //I am thinking about writting a function that would be able to calculate the average
+  //so this part could be better refactored 
+
+  getMostActivePeople(usersData, date){
+    const listByDate = this.data.filter(entry => entry.date === date);
+    const getIDs = listByDate.reduce((names, entry) => { 
+        if(entry.minutesActive > 250){
+            names.push(entry.userID)
+        }
+        return names
+    }, [])
+    // console.log(getIDs)
+    const getNames = getIDs.map(id => {
+        const user = usersData.find(user => user.id === id);
+        console.log(user)
+        return user.name
+    })
+    return getNames
+  }
+  
 }
 
 if (typeof module !== 'undefined') {
   module.exports = ActivityRepository;
 }
+/*
+  getMostActivePeople(usersData, date){
+    const listByDate = this.data.filter(entry => entry.date === date);
+    const getIDs = listByDate.reduce((names, entry) => { 
+        if(entry.minutesActive > 250){
+            names.push(entry.userID)
+        }
+        return names
+    }, [])
+    console.log(getIDs)//[1, 4]
+    console.log(usersData)
+
+    return getIDs
+  }
+*/
