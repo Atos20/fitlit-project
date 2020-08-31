ee7128dfce36606bfe7189c2ecd05951d51cddcf
Orlando Murcio <ato.murcio@gmail.com>
2020-08-31 16:55:02 -0600

    Add methods that will get the informstion from the person who've walked the most

--- src/user.js
+++ src/user.js
@@ -13,11 +13,47 @@ class User {
     const firstName = this.name.split(' ');
     return firstName[0];
   }
-  
-  retrieveFriendsList(user){
-    
+
+  retrieveFriendsList(user, usersData){
+    const friendsList = user.friends.map( friendId => {
+      const locateInformation = usersData.find(entry => entry.id === friendId)
+      return locateInformation
+    })
+    return friendsList
   }
 
+  getUserStepCount(user, activityData, date) {
+    const userDesiredDate = activityData.filter(entry => entry.userID=== user.id);
+    const locateDate = userDesiredDate.find( entry => entry.date === date);
+    const findIndex = userDesiredDate.indexOf(locateDate);
+    const getDesiredWeek = userDesiredDate.slice(findIndex -6, findIndex +1);
+    const getAverageStepCount = getDesiredWeek.reduce((totalAverage, userData) => {
+      totalAverage += userData.numSteps
+      return totalAverage
+    }, 0)
+    return getAverageStepCount
+  }
+
+  getFriendsStepCount(user, usersData, activityData, date) {
+    const friendsList = this.retrieveFriendsList(user, usersData);
+    const  weeklyFriendsStepAverage = friendsList.map(friend => { 
+      const total = this.getUserStepCount(friend, activityData, date)
+      // console.log(friend.name)
+      // const firstName = friend.name.split(' ')[0];
+      // console.log(friend.id)
+      return { 'id': friend.id, 'averageStep':total};
+    })
+    return weeklyFriendsStepAverage
+  }
+
+  getBestWalkersData(user, usersData, activityData, date) {
+    const friendsWeeklyStepsAverage = this.getFriendsStepCount(user, usersData, activityData, date);
+    const sortedFriendsByStepCount = friendsWeeklyStepsAverage.sort((a, b) => b.averageStep - a.averageStep);
+    const bestWalker = sortedFriendsByStepCount[0];
+    const getWinnersData = usersData.find(entry => entry.id === bestWalker.id);
+    console.log(getWinnersData)
+    return getWinnersData
+  }
 }
 
 if (typeof module !== 'undefined') {
