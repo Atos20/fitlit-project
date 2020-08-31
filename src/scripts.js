47f9f44f1b198fa4b0af37e19a5a5b53574ba7f1
Orlando Murcio <ato.murcio@gmail.com>
2020-08-30 20:24:13 -0600

    Remove unnecessary solors from weekly activity chart

--- src/scripts.js
+++ src/scripts.js
@@ -137,6 +137,8 @@ let displayDailyAndAverageSleepData = (user, today) => {
         }]
     },
     options: {
+      responsive: true,
+      maintainAspectRatio: true,
         scales: {
             yAxes: [{
                 ticks: {
@@ -195,7 +197,7 @@ let weeklySleepQualityAndSleepHours = (user, today) => {
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 1,
         },{
             label: 'Sleep Quality', 
             data: data2,
@@ -294,6 +296,7 @@ let displayDailyActivityVsAll = (user, date) => {
         }]
     },
     options: {
+      
         scales: {
             yAxes: [{
                 ticks: {
@@ -325,62 +328,40 @@ let displayWeeklyActivity = (user, date) => {
             label: 'Miles',
             data: weeklyMiles,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
+
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(54, 162, 235, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 1,
+            fill: false,
+            lineTension: 0
         },{
             label: 'Active Minutes',
             data: weeklyActiveMin,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 1,
+            fill: false,
+            lineTension: 0
         },{
             label: 'Flights',
             data: weeklyFlights,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
+,
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 1,
+            fill: false,
+            lineTension: 0
         }]
     },
     options: {
