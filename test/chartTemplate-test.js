ead953f7d129c31ef236d282a419136d070894f1
Orlando Murcio <ato.murcio@gmail.com>
2020-08-31 19:34:10 -0600

    Add before block

--- test/chartTemplate-test.js
+++ test/chartTemplate-test.js
@@ -1,3 +1,15 @@
 const chai = require('chai');
 const expect = chai.expect;
+const chartTemplate = require('..src/chartTemplate.js')
 
+describe('User', () => {
+
+  let chart;
+
+  beforeEach(() => {
+    chart = new Chart(type, label, title, data)
+  })
+  it('it should be a function', () => {
+      console.log(chart)
+  })
+})
