50d71cfab46932ab37f20180db8a5576df76e966
Orlando Murcio <ato.murcio@gmail.com>
2020-08-31 20:01:46 -0600

    Instantiate two chartTemplates

--- test/chartTemplate-test.js
+++ test/chartTemplate-test.js
@@ -4,15 +4,22 @@ const ChartTemplate = require('../src/chartTemplate')
 
 describe('User', () => {
 
-  let chart;
-
+  let chartTemplate1;
+  let chartTemplate2;
   beforeEach(() => {
-    chart = new ChartTemplate()
+    chartTemplate1 = new ChartTemplate('bar', ['one', 'two'], 'this is a bar chart', [])
+    chartTemplate2 = new ChartTemplate('line', ['three', 'four'], 'this is a line chart', [])
   })
   it('it should be a function', () => {
     expect(ChartTemplate).to.be.a('function');
   });
   it('should be an instance of the charTemplate class', () => {
-    expect(chart).to.be.an.instanceOf(ChartTemplate);
-  })
+    expect(chartTemplate1).to.be.an.instanceOf(ChartTemplate);
+  });
+  it('should be able to create a bar chart',() => {
+    expect(chartTemplate1.type).to.deep.equal('bar');
+  });
+  it('should be able to create a line chart', () => {
+    expect(chartTemplate2.type).to.equal('line');
+  });
 })
